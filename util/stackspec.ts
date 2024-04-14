d7849ec5333d4d006d9a22953707da175bb7d901
Alexander Mattoni <5110855+mattoni@users.noreply.github.com>
2024-04-13 22:16:35 -0700

    add stack spec schema generation

---
+++ util/stackspec.ts
@@ -0,0 +1,22 @@
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+import fs from "fs";
+import path from "path";
+
+async function bundleStackSpecSchema() {
+  try {
+    const parser = new $RefParser();
+
+    const input = path.join(
+      __dirname,
+      "../components/schemas/stacks/spec/StackSpec.yml"
+    );
+
+    const output = path.join(__dirname, "../dist/stackspec.json");
+
+    const schema = await parser.bundle(input, { mutateInputSchema: false });
+    fs.writeFileSync(output, JSON.stringify(schema));
+  } catch (err) {
+    console.error(err);
+  }
+}
+bundleStackSpecSchema();
